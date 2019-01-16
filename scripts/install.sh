44d0b32f8ac96751fc23d97d6be4e48587b0fbf8
Ady Romantika <ady.romantika@gmail.com>
2019-01-16 21:40:49 +0800

    Initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if grep -i ubuntu /proc/version >/dev/null 2>&1; then
+  sudo apt update
+  sudo apt install -y zsh zsh-common
+elif uname | grep -i darwin >/dev/null 2>&1; then
+  if \which brew >/dev/null 2>&1; then
+    brew install zsh zsh-completions
+  fi
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mkdir -p ~/.oh-my-zsh/custom/plugins
+mkdir -p ~/.oh-my-zsh/custom/themes
+
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-aws-vault ]; then
+  cd ~/.oh-my-zsh/custom/plugins/zsh-aws-vault && git pull
+else
+  git clone git@github.com:adyromantika/zsh-aws-vault.git ~/.oh-my-zsh/custom/plugins/zsh-aws-vault
+fi
+
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-nvm ]; then
+  cd ~/.oh-my-zsh/custom/plugins/zsh-nvm && git pull
+else
+  git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+fi
+
+if [ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
+  cd ~/.oh-my-zsh/custom/themes/powerlevel9k && git pull
+else
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
