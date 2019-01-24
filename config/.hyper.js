a6b56af8823be5d0c08e7a2a15a029dd8daec8a3
Ady Romantika <ady.romantika@gmail.com>
2019-01-24 18:26:15 +0800

    Remove unused plugin

--- config/.hyper.js
+++ config/.hyper.js
@@ -140,7 +140,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-material-theme", "hyper-solarized-dark2"],
+  plugins: ["hyper-solarized-dark2"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
