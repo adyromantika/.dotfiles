4943ceb750796ceaf8d0705524c4cf880844f3dd
Ady Romantika (2012) <ady.romantika@gmail.com>
2026-02-17 13:40:48 +0800

    Use https

--- scripts/bat.sh
+++ scripts/bat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /tmp/bat
-git clone git@github.com:catppuccin/bat.git /tmp/bat
+git clone https://github.com/catppuccin/bat.git /tmp/bat
 mkdir -p "$(bat --config-dir)/themes"
 cp /tmp/bat/themes/*.tmTheme "$(bat --config-dir)/themes"
 bat cache --build
